e952e4b96b169f72d9c25f0295384bea7f3cd24b
Alisher <shodikulzoda@gmail.com>
2025-03-03 11:19:43 +0500

    test

--- Program.cs
+++ Program.cs
@@ -4,6 +4,31 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("first num:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("second num:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("choose the type of operation (+ or -):");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        int result;
+
+        if (operation == '+')
+        {
+            result = num1 + num2;
+            Console.WriteLine($"result: {num1} + {num2} = {result}");
+        }
+        else if (operation == '-')
+        {
+            result = num1 - num2;
+            Console.WriteLine($"result: {num1} - {num2} = {result}");
+        }
+        else
+        {
+            Console.WriteLine("Wrong input");
+        }
+        
     }
 }
